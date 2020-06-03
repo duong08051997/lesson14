2c042b4defbee2283fdaee7ea183814aafdbe0c9
buixuanduong <duong08051997@gmail.com>
2020-06-03 23:29:28 +0900

    bài 14- hàm

---
+++ js/demo.js
@@ -0,0 +1,10 @@
+function square(number) {
+  let  result = number*number;
+  return result;
+
+
+}
+let squareOf2 = square(2);
+console.log(squareOf2 );
+let squareOf5 = square(5);
+console.log(squareOf5);
